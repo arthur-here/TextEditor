86dac13b62c4b000b5678d4b82203bc67dc6064e
burntheroad <gibslp69@gmail.com>
2015-09-19 20:05:19 +0300

    Fix nuget packages configuration

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -3,12 +3,12 @@
 namespace TextEditor
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml.
     /// </summary>
     public partial class MainWindow : Window
     {
         /// <summary>
-        /// Interaction logic for MainWindow.xaml
+        /// Initializes a new instance.
         /// </summary>
         public MainWindow()
         {
