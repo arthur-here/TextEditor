41fb457023ce43b558dce442d7e07ce24b671cef
burntheroad <gibslp69@gmail.com>
2015-10-04 22:46:29 +0300

    Added Utf8FileReader

---
+++ TextEditor/FileSaveLoad/UTF8FileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Provides agorythm to reading UTF-8 encoded file
+    /// </summary>
+    public class Utf8FileReader : FileReaderStrategy
+    {
+        public Utf8FileReader(string fileName)
+        {
+            this.FileName = fileName;
+        }
+
+        public override string[] ReadFile()
+        {
+            List<string> result = new List<string>();
+            using (System.IO.StreamReader streamReader = new System.IO.StreamReader(this.FileName, UTF8Encoding.UTF8))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    result.Add(streamReader.ReadLine());
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
