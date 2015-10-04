1eac861090edf47ac09ed10bc9795da93308527f
burntheroad <gibslp69@gmail.com>
2015-10-04 21:55:27 +0300

    Added IFileReader interface

---
+++ TextEditor/FileSaveLoad/IFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    public interface IFileReader
+    {
+        string Filename { get; set; }
+
+        string[] ReadFile();
+    }
+}
