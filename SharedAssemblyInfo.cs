824bb48a398fb2314d1279ba1df348680c1cdd11
burntheroad <gibslp69@gmail.com>
2015-10-12 18:57:09 +0300

    Created SourceTextBox user control and replaced RichTextBox

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -32,12 +32,12 @@ using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located
-#pragma warning disable SA1115 // Parameter must follow comma
+// Parameter must follow comma
     // (used if a resource is not found in the page, 
     // or application resource dictionaries)
 
     ResourceDictionaryLocation.SourceAssembly)] // where the generic resource dictionary is located
-#pragma warning restore SA1115 // Parameter must follow comma
+// Parameter must follow comma
 // (used if a resource is not found in the page, 
 // app, or any theme specific resource dictionaries)
 
---
+++ TextEditor/SourceTextBox.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TextEditor.FileManager;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for SourceTextBox.
+    /// </summary>
+    public partial class SourceTextBox : TextBox
+    {
+        private TextEditorDocument document;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SourceTextBox"/> class.
+        /// </summary>
+        public SourceTextBox()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets <see cref="TextEditorDocument"/>, which displayed by <see cref="SourceTextBox"/>.
+        /// </summary>
+        public TextEditorDocument Document
+        {
+            get
+            {
+                return this.document;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    return;
+                }
+
+                this.document = value;
+                this.Text = string.Join("\n", this.document.Lines);
+            }
+        }
+    }
+}
