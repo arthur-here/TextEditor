34c16a9c698300d5e85e052018219494b5845acb
burntheroad <gibslp69@gmail.com>
2015-10-12 17:13:06 +0300

    Added Command interface and unfinished class InsertStringCommand

---
+++ TextEditor/Commands/ICommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor.Commands
+{
+    /// <summary>
+    /// Provides interface for Command.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Executes command.
+        /// </summary>
+        void Execute();
+
+        /// <summary>
+        /// Unexecutes command.
+        /// </summary>
+        void Undo();
+    }
+}
---
+++ TextEditor/Commands/InsertStringCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using TextEditor.FileManager;
+
+namespace TextEditor.Commands
+{
+    class InsertStringCommand : ICommand
+    {
+        private TextEditorDocument document;
+        private int line;
+        private int position;
+
+        public InsertStringCommand(TextEditorDocument document, int line, int position)
+        {
+            this.document = document;
+            this.line = line;
+            this.position = position;
+        }
+
+        public void Execute()
+        {
+            Paragraph paragraph = document.Blocks.ElementAt(line) as Paragraph;
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
