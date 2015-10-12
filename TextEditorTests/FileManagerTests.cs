a731b9eca6be0fe6e457cc32b4da25f9e58db2a7
burntheroad <gibslp69@gmail.com>
2015-10-12 13:38:45 +0300

    Fixed ruleset in Release configuration

--- TextEditorTests/FileManagerTests.cs
+++ TextEditorTests/FileManagerTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TextEditor.FileManager;
 using System.Windows.Documents;
 using System.Text;
