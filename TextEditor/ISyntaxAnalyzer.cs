2d48e7b46641160f8d9396db522c5b5d9869d534
burntheroad <gibslp69@gmail.com>
2015-11-29 22:44:48 +0200

    Added SyntaxAnalyser interface and simple Swift implementaton

---
+++ TextEditor/ISyntaxAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Provides interface to syntax analyzers.
+    /// </summary>
+    public interface ISyntaxAnalyzer
+    {
+        /// <summary>
+        /// Gets list of parsed tokens from text.
+        /// </summary>
+        List<Token> Tokens { get; }
+
+        /// <summary>
+        /// Parse document text and saves result in Tokens.
+        /// </summary>
+        /// <param name="document">Document to analyze.</param>
+        void Parse(TextEditorDocument document);
+    }
+}
---
+++ TextEditor/Token.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{   
+    /// <summary>
+    /// Enumeration that represents all type of available tokens: Comment, Number, Keyword.
+    /// </summary>
+    public enum TokenType
+    {
+        Comment, Number, Keyword
+    }
+
+    /// <summary>
+    /// Encapsulates information about syntax token in text.
+    /// </summary>
+    public class Token
+    {
+        private TokenType type;
+        private int caretIndex;
+        private int length;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Token"/> class.
+        /// </summary>
+        /// <param name="type"><see cref="TokenType"/> of token.</param>
+        /// <param name="caretIndex">Start position in text.</param>
+        /// <param name="length">Length of token.</param>
+        public Token(TokenType type, int caretIndex, int length)
+        {
+            this.type = type;
+            this.caretIndex = caretIndex;
+            this.length = length;
+        }
+
+        /// <summary>
+        /// Gets <see cref="TokenType"/> of this token.
+        /// </summary>
+        public TokenType TokenType
+        {
+            get { return this.type; }
+        }
+
+        /// <summary>
+        /// Gets or sets position of token in text.
+        /// </summary>
+        public int CaretIndex
+        {
+            get { return this.caretIndex; }
+            set { this.caretIndex = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets length of token string.
+        /// </summary>
+        public int Length
+        {
+            get { return this.length; }
+            set { this.length = value; }
+        }
+    }
+}
